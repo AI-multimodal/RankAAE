9bd727b498ed09915f71c472107c34af25f48698
Xiaohui Qu <xiaqu@bnl.gov>
2020-08-07 16:57:26 -0400

    add setup.py to support "develop" mode pointing installation to a local directory. add requirements.txt to specify the required libraries.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f_req:
+    required_list = [line.rstrip() for line in f_req.readlines()]
+
+
+setup(
+    name='sc',
+    version='0.1',
+    packages=find_packages(where="sc"),
+    url='https://www.bnl.gov/cfn/',
+    license='GPL',
+    author='xiaohuiqu',
+    author_email='xiaqu@bnl.gov; mrc2215@columbia.edu',
+    description='Semi-supervised Clustering',
+    python_requires='>=3.7',
+    install_requires=required_list
+)
