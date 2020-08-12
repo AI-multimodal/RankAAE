ba409e83850482fc9565cd65768c1585623ec66c
Xiaohui Qu <xiaqu@bnl.gov>
2020-08-12 12:05:45 -0400

    change opt_hyper.py to single process and enable multiple processes using GNU parallel

--- examples/opt_hyper.py
+++ examples/opt_hyper.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
 import argparse
-import socket
-from contextlib import contextmanager
-import multiprocessing
-from optuna.trial import Trial
-from sc.clustering.trainer import Trainer
-import optuna
 import os
-import yaml
-import time
-import numpy as np
-from optuna.pruners import HyperbandPruner
+import socket
 import subprocess
+import time
+import platform
+
+import numpy as np
+import optuna
+import yaml
+from optuna.pruners import HyperbandPruner
+from optuna.trial import Trial
+
+from sc.clustering.trainer import Trainer
 
 
 class TrainerCallBack:
@@ -33,26 +34,11 @@ class TrainerCallBack:
             raise optuna.TrialPruned()
 
 
-class GpuQueue:
-    def __init__(self, n_gpus, n_jobs):
-        self.queue = multiprocessing.Manager().Queue()
-        all_idxs = list(range(n_jobs)) if n_gpus > 0 else [None]
-        self.n_gpus = n_gpus
-        for idx in all_idxs:
-            self.queue.put(idx)
-
-    @contextmanager
-    def one_gpu_per_process(self):
-        current_idx = self.queue.get()
-        yield current_idx % self.n_gpus
-        self.queue.put(current_idx)
-
-
 class Objective:
-    def __init__(self, gpu_queue: GpuQueue, trainer_args, opt_config, base_trail_number,
+    def __init__(self, igpu, trainer_args, opt_config, base_trail_number,
                  single_objective, merge_objectives):
         super().__init__()
-        self.gpu_queue = gpu_queue
+        self.igpu = igpu
         self.trainer_args = trainer_args
         self.opt_config = opt_config
         self.base_trail_number = base_trail_number
@@ -60,38 +46,39 @@ class Objective:
         self.merge_objectives = merge_objectives
 
     def __call__(self, trial: Trial):
-        with self.gpu_queue.one_gpu_per_process() as gpu_i:
-            kwargs = {}
-            for k, v in self.opt_config.items():
-                if v["sampling"] != 'the categorical':
-                    low, high = v["low"], v["high"]
-                if v["sampling"] == 'int':
-                    kwargs[k] = trial.suggest_int(name=k, low=low, high=high)
-                elif v["sampling"] == 'uniform':
-                    kwargs[k] = trial.suggest_uniform(name=k, low=low, high=high)
-                elif v["sampling"] == 'loguniform':
-                    kwargs[k] = trial.suggest_loguniform(name=k, low=low, high=high)
-                elif v["sampling"] == 'categorical':
-                    kwargs[k] = trial.suggest_categorical(name=k, choices=v["choices"])
-            work_dir = f'{os.path.expandvars(os.path.expanduser(self.trainer_args.work_dir))}/trials' \
-                       f'/{self.base_trail_number+trial.number:05d}_{time.time_ns() - 1597090000000000000}'
-            trainer = Trainer.from_data(self.trainer_args.data_file,
-                                        igpu=gpu_i,
-                                        max_epoch=self.trainer_args.max_epoch,
-                                        verbose=self.trainer_args.verbose,
-                                        work_dir=work_dir,
-                                        **kwargs)
-            if self.single_objective:
-                trainer_callback = TrainerCallBack(self.merge_objectives, trial)
+        kwargs = {}
+        for k, v in self.opt_config.items():
+            if v["sampling"] != 'the categorical':
+                low, high = v["low"], v["high"]
             else:
-                trainer_callback = None
-            metrics = trainer.train(trainer_callback)
-            if self.single_objective:
-                if self.merge_objectives:
-                    weights = [100, 50.0, 50.0, -1.0, -1.0, -1.0, -1.0]
-                    metrics = (np.array(weights) * np.array(metrics)).sum()
-                else:
-                    metrics = metrics[0]
+                low, high = None, None
+            if v["sampling"] == 'int':
+                kwargs[k] = trial.suggest_int(name=k, low=low, high=high)
+            elif v["sampling"] == 'uniform':
+                kwargs[k] = trial.suggest_uniform(name=k, low=low, high=high)
+            elif v["sampling"] == 'loguniform':
+                kwargs[k] = trial.suggest_loguniform(name=k, low=low, high=high)
+            elif v["sampling"] == 'categorical':
+                kwargs[k] = trial.suggest_categorical(name=k, choices=v["choices"])
+        work_dir = f'{os.path.expandvars(os.path.expanduser(self.trainer_args.work_dir))}/trials' \
+                   f'/{self.base_trail_number+trial.number:05d}_{time.time_ns() - 1597090000000000000}'
+        trainer = Trainer.from_data(self.trainer_args.data_file,
+                                    igpu=self.igpu,
+                                    max_epoch=self.trainer_args.max_epoch,
+                                    verbose=self.trainer_args.verbose,
+                                    work_dir=work_dir,
+                                    **kwargs)
+        if self.single_objective:
+            trainer_callback = TrainerCallBack(self.merge_objectives, trial)
+        else:
+            trainer_callback = None
+        metrics = trainer.train(trainer_callback)
+        if self.single_objective:
+            if self.merge_objectives:
+                weights = [100, 50.0, 50.0, -1.0, -1.0, -1.0, -1.0]
+                metrics = (np.array(weights) * np.array(metrics)).sum()
+            else:
+                metrics = metrics[0]
         return metrics
 
 
@@ -107,10 +94,8 @@ def main():
                         help='Maximum iterations')
     parser.add_argument('-w', "--work_dir", type=str, default='.',
                         help="Working directory to write the output files")
-    parser.add_argument('-g', '--gpus', type=int, default=4,
-                        help='Number of GPUs')
-    parser.add_argument('-j', '--jobs', type=int, default=8,
-                        help='Number of Optuna trial jobs')
+    parser.add_argument('-g', '--gpu_i', type=int, default=0,
+                        help='ID for GPU to use')
     parser.add_argument('-t', '--trials', type=int, default=50,
                         help='Number of total trails to evaluate model')
     parser.add_argument('--name', type=str, default='opt_daae',
@@ -134,15 +119,16 @@ def main():
         print(f"Task ID is {task_id}, will sleep {sleep_seconds} seconds before start")
         time.sleep(sleep_seconds)
 
-    hostname = socket.gethostname().split('.', 1)[0]
-    if not os.path.exists(f'{work_dir}/resource_usage_{job_id}'):
-        os.makedirs(f'{work_dir}/resource_usage_{job_id}', exist_ok=True)
-    cpu_log_file = open(f'{work_dir}/resource_usage_{job_id}/cpu_{task_id}_{hostname}.txt', 'wt')
-    subprocess.Popen(['/usr/bin/top', '-i', '-b', '-d', '31'],
-                     stdout=cpu_log_file)
-    gpu_log_file = open(f'{work_dir}/resource_usage_{job_id}/gpu_{task_id}_{hostname}.txt', 'wt')
-    subprocess.Popen(['/usr/bin/nvidia-smi', '-l', '37'],
-                     stdout=gpu_log_file)
+    if platform.system() == 'Linux':
+        hostname = socket.gethostname().split('.', 1)[0]
+        if not os.path.exists(f'{work_dir}/resource_usage_{job_id}'):
+            os.makedirs(f'{work_dir}/resource_usage_{job_id}', exist_ok=True)
+        cpu_log_file = open(f'{work_dir}/resource_usage_{job_id}/cpu_{task_id}_{hostname}.txt', 'wt')
+        subprocess.Popen(['/usr/bin/top', '-i', '-b', '-d', '31'],
+                         stdout=cpu_log_file)
+        gpu_log_file = open(f'{work_dir}/resource_usage_{job_id}/gpu_{task_id}_{hostname}.txt', 'wt')
+        subprocess.Popen(['/usr/bin/nvidia-smi', '-l', '37'],
+                         stdout=gpu_log_file)
 
     with open(args.config) as f:
         opt_config = yaml.full_load(f)
@@ -166,9 +152,8 @@ def main():
             storage=f'sqlite:///{work_dir}/{args.name}.db',
             load_if_exists=True)
     base_trail_number = len(study.trials)
-    gq = GpuQueue(args.gpus, args.jobs)
-    obj = Objective(gq, args, opt_config, base_trail_number, single_objective, merge_objectives)
-    study.optimize(obj, n_trials=args.trials, n_jobs=args.jobs)
+    obj = Objective(args.gpu_i, args, opt_config, base_trail_number, single_objective, merge_objectives)
+    study.optimize(obj, n_trials=args.trials)
 
     print("Number of finished trials: ", len(study.trials))
     if single_objective:
---
+++ examples/opt_hyper.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+num_jobs=$1
+sh_fn=`realpath ${BASH_SOURCE}`
+py_fn=${sh_fn/.sh/.py}
+
+export CONDA_SHLVL=1
+export CONDA_PROMPT_MODIFIER=(/hpcgpfs01/software/cfn-jupyter/software/xas_m    l)
+export CONDA_EXE=/sdcc/u/xiaqu/program/miniconda3/bin/conda
+export PATH=/hpcgpfs01/software/cfn-jupyter/software/xas_ml/bin:$PATH
+export CONDA_PREFIX=/hpcgpfs01/software/cfn-jupyter/software/xas_ml
+export CONDA_PYTHON_EXE=/sdcc/u/xiaqu/program/miniconda3/bin/python
+export CONDA_DEFAULT_ENV=/hpcgpfs01/software/cfn-jupyter/software/xas_ml
+
+res_dir=resources_usage_${SLURM_JOB_ID}
+if [[ ! -d ${res_dir} ]]
+then
+    mkdir "${res_dir}"
+fi
+
+nvida-smi -l 37 &> ${res_dir}/gpu_${SLURM_PROCID}_`hostname` &
+top -i -b -d 31 &> ${res_dir}/cpu_${SLURM_PROCID}_`hostname` &
+
+log_dir=optuna_run_${SLURM_JOB_ID}
+if [[ ! -d ${log_dir} ]]
+then
+    mkdir "${log_dir}"
+fi
+
+seq ${num_jobs} | parallel -j ${num_jobs} "
+sleep \$(( ({#}-1) * 3 ))
+python ${py_fn} -g \$(({%} % 4)) ${@:2}
+"
