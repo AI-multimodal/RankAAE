ee7d9f9ca5a488f92f25b8c38112fda2ba45a6bb
Xiaohui Qu <xiaqu@bnl.gov>
2020-08-12 22:09:14 -0400

    detect GPU type and set number of GPU dynamically

--- examples/opt_hyper.sh
+++ examples/opt_hyper.sh
@@ -5,6 +5,19 @@ head_node=$2   ## can be obtained from ${SLURMD_NODENAME}
 sh_fn=$(realpath "${BASH_SOURCE}")
 py_fn=${sh_fn/.sh/.py}
 
+gpu_type=$(srun nvidia-smi | head -n 8 | tail -n 1 | tr -s ' ' | cut -d ' ' -f 4)
+gpu_type=${gpu_type%-*}
+
+if [[ ${gpu_type} == "P100" ]]
+then
+    num_gpus=2
+elif [[ ${gpu_type} == "K80" ]]
+then
+    num_gpus=4
+else
+    num_gpus=-1
+fi
+
 export CONDA_SHLVL=1
 export CONDA_PROMPT_MODIFIER=(/hpcgpfs01/software/cfn-jupyter/software/xas_ml)
 export CONDA_EXE=/sdcc/u/xiaqu/program/miniconda3/bin/conda
@@ -38,7 +51,7 @@ fi
 
 seq ${num_jobs} | parallel -j ${num_jobs} "
 sleep \$(( ({#}-1) * 3 + (${SLURM_PROCID} * ${num_jobs} + 1) * 3 ))
-python ${py_fn} -g \$(({%} % 4)) -a ${head_node} ${@:3} &> ${log_dir}/opt_${SLURM_PROCID}_{#}_${hn}.txt
+python ${py_fn} -g \$(({%} % ${num_gpus})) -a ${head_node} ${@:3} &> ${log_dir}/opt_${SLURM_PROCID}_{#}_${hn}.txt
 "
 
 redis-cli -h ${hn} shutdown
