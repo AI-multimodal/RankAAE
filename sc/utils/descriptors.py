d20dd4fea9b69698f008aac62f3efd4f6a1b0f1d
Xiaohui Qu <xhqu1981@gmail.com>
2021-03-29 17:10:38 -0400

    Add AngularFourierSeries descriptor

---
+++ sc/utils/descriptors.py
@@ -0,0 +1,126 @@
+from matminer.featurizers.base import BaseFeaturizer
+
+class AngularFourierSeries(BaseFeaturizer):
+    """
+    Compute the angular Fourier series (AFS), including both angular and radial info
+    The AFS is the product of pairwise distance function (g_n, g_n') between two pairs
+    of atoms (sharing the common central site) and the cosine of the angle
+    between the two pairs. The AFS is a 2-dimensional feature (the axes are g_n,
+    g_n').
+    Examples of distance functionals are square functions, Gaussian, trig
+    functions, and Bessel functions. An example for Gaussian:
+        lambda d: exp( -(d - d_n)**2 ), where d_n is the coefficient for g_n
+    See :func:`~matminer.featurizers.utils.grdf` for a full list of available binning functions.
+    There are two preset conditions:
+        gaussian: bin functions are gaussians
+        histogram: bin functions are rectangular functions
+    Features:
+        AFS ([gn], [gn']) - Angular Fourier Series between binning functions (g1 and g2)
+    Args:
+        bins:   ([AbstractPairwise]) a list of binning functions that
+                implement the AbstractPairwise base class
+        cutoff: (float) maximum distance to look for neighbors. The
+                 featurizer will run slowly for large distance cutoffs
+                 because of the number of neighbor pairs scales as
+                 the square of the number of neighbors
+    """
+
+    def __init__(self, bins, cutoff=10.0):
+        self.bins = bins
+        self.cutoff = cutoff
+
+    def featurize(self, struct, idx):
+        """
+        Get AFS of the input structure.
+        Args:
+            struct (Structure): Pymatgen Structure object.
+            idx (int): index of target site in structure struct.
+        Returns:
+            Flattened list of AFS values. the list order is:
+                g_n g_n'
+        """
+
+        if not struct.is_ordered:
+            raise ValueError("Disordered structure support not built yet")
+
+        # Generate list of neighbor position vectors (relative to central
+        # atom) and distances from each central site as tuples
+        sites = struct._sites
+        central_site = sites[idx]
+        neighbors_lst = struct.get_neighbors(central_site, self.cutoff)
+        neighbor_collection = [
+            (neighbor[0].coords - central_site.coords, neighbor[1])
+            for neighbor in neighbors_lst]
+
+        # Generate exhaustive permutations of neighbor pairs around each
+        # central site (order matters). Does not allow repeat elements (i.e.
+        # there are two distinct sites in every permutation)
+        neighbor_tuples = itertools.permutations(neighbor_collection, 2)
+
+        # Generate cos(theta) between neighbor pairs for each central site.
+        # Also, retain data on neighbor distances for each pair
+        # process with matrix algebra, we really need the speed here
+        data = np.array(list(neighbor_tuples))
+        v1, v2 = np.vstack(data[:, 0, 0]), np.vstack(data[:, 1, 0])
+        distances = data[:, :, 1]
+        neighbor_pairs = np.concatenate([
+            np.clip(np.einsum('ij,ij->i', v1, v2) /
+                    np.linalg.norm(v1, axis=1) /
+                    np.linalg.norm(v2, axis=1), -1.0, 1.0).reshape(-1, 1),
+            distances], axis=1)
+
+        # Generate distance functional matrix (g_n, g_n')
+        bin_combos = list(itertools.product(self.bins, repeat=2))
+
+        # Compute AFS values for each element of the bin matrix
+        # need to cast arrays as floats to use np.exp
+        cos_angles, dist1, dist2 = neighbor_pairs[:, 0].astype(float),\
+            neighbor_pairs[:, 1].astype(float),\
+            neighbor_pairs[:, 2].astype(float)
+        features = [sum(combo[0](dist1) * combo[1](dist2) *
+                        cos_angles) for combo in bin_combos]
+
+        return features
+
+    def feature_labels(self):
+        bin_combos = list(itertools.product(self.bins, repeat=2))
+        return ['AFS ({}, {})'.format(combo[0].name(), combo[1].name())
+                for combo in bin_combos]
+
+    @staticmethod
+    def from_preset(preset, width=0.5, spacing=0.5, cutoff=10):
+        """
+        Preset bin functions for this featurizer. Example use:
+            >>> AFS = AngularFourierSeries.from_preset('gaussian')
+            >>> AFS.featurize(struct, idx)
+        Args:
+            preset (str): shape of bin (either 'gaussian' or 'histogram')
+            width (float): bin width. std dev for gaussian, width for histogram
+            spacing (float): the spacing between bin centers
+            cutoff (float): maximum distance to look for neighbors
+        """
+
+        # Generate bin functions
+        if preset == "gaussian":
+            bins = []
+            for center in np.arange(0., cutoff, spacing):
+                bins.append(Gaussian(width, center))
+        elif preset == "histogram":
+            bins = []
+            for start in np.arange(0, cutoff, spacing):
+                bins.append(Histogram(start, width))
+        else:
+            raise ValueError('Not a valid preset condition.')
+        return AngularFourierSeries(bins, cutoff=cutoff)
+
+    def citations(self):
+        return ['@article{PhysRevB.95.144110, title = {Representation of compo'
+                'unds for machine-learning prediction of physical properties},'
+                ' author = {Seko, Atsuto and Hayashi, Hiroyuki and Nakayama, '
+                'Keita and Takahashi, Akira and Tanaka, Isao},'
+                'journal = {Phys. Rev. B}, volume = {95}, issue = {14}, '
+                'pages = {144110}, year = {2017}, publisher = {American Physic'
+                'al Society}, doi = {10.1103/PhysRevB.95.144110}}']
+
+    def implementors(self):
+        return ["Maxwell Dylla", "Logan Williams"]
