483046823caca429889344e6fb158207300fab0b
Xiaohui Qu <xhqu1981@gmail.com>
2021-04-02 17:25:56 -0400

    Fix the length

--- sc/post_hoc_explanation/latent2angularpdf/tests/test_lat2ang_dataloader.py
+++ sc/post_hoc_explanation/latent2angularpdf/tests/test_lat2ang_dataloader.py
@@ -7,8 +7,9 @@ data_fn = os.path.join(os.path.dirname(__file__), "../../../../data", "mini_late
 
 class TestLatent2AngularPDFDataset(TestCase):
     def test_dataloader(self):
-        dl_train, dl_val, dl_test = get_latent2apdf_dataloaders(data_fn, 512)
-        self.assertEqual(len(dl_train), 32)
-        self.assertEqual(len(dl_val), 32)
+        dl_train, dl_val, dl_test = get_latent2apdf_dataloaders(data_fn, 4)
+        self.assertEqual(len(dl_train), 8)
+        self.assertEqual(len(dl_val), 8)
+        self.assertEqual(len(dl_test), 8)
         self.assertEqual(len(dl_test.dataset), 32)
 
