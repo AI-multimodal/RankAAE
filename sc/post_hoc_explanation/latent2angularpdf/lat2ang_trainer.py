9de260c8153d3cc074146f0155365e4bbcd5747d
Xiaohui Qu <xhqu1981@gmail.com>
2021-04-04 18:17:38 -0400

    change work dir

--- sc/post_hoc_explanation/latent2angularpdf/lat2ang_trainer.py
+++ sc/post_hoc_explanation/latent2angularpdf/lat2ang_trainer.py
@@ -188,7 +188,7 @@ class Latent2AngularPDFTrainer:
         model = model.to(device)
         trainer = Latent2AngularPDFTrainer(model, device, dl_train, dl_val, lr=lr, max_epoch=max_epoch, sch_factor=sch_factor,
                                            sch_patience=sch_patience, style_noise=style_noise, weight_decay=weight_decay, 
-                                           optimizer_name=optimizer_name, verbose=verbose, work_dir='.')
+                                           optimizer_name=optimizer_name, verbose=verbose, work_dir=work_dir)
         return trainer
 
     @classmethod
