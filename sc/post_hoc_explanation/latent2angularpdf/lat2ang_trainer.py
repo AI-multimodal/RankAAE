375ad353fd3176f630036dcbbfa63df05721ea99
Xiaohui Qu <xhqu1981@gmail.com>
2021-04-04 17:55:21 -0400

    add plotting test

--- sc/post_hoc_explanation/latent2angularpdf/lat2ang_trainer.py
+++ sc/post_hoc_explanation/latent2angularpdf/lat2ang_trainer.py
@@ -52,31 +52,33 @@ class Latent2AngularPDFTrainer:
             with torch.no_grad():
                 self.tb_writer.add_graph(self.model.cpu(), example_lat)
     
-    def precompute_latent_variation(self):
-        one_hot_variation = torch.eye(self.nclasses).unsqueeze(dim=1).repeat([1, self.nstyle, 1])\
-            .unsqueeze(dim=2).repeat([1, 1, self.ntest_image_per_style, 1])
-        style_variation = torch.zeros([self.nclasses, self.nstyle, self.ntest_image_per_style, self.nstyle])
-        style_variation[:, torch.arange(self.nstyle), :, torch.arange(self.nstyle)] = torch.linspace(-2, 2, self.ntest_image_per_style)
+    @staticmethod
+    def precompute_latent_variation(nclasses, nstyle, ntest_image_per_style):
+        one_hot_variation = torch.eye(nclasses).unsqueeze(dim=1).repeat([1, nstyle, 1])\
+            .unsqueeze(dim=2).repeat([1, 1, ntest_image_per_style, 1])
+        style_variation = torch.zeros([nclasses, nstyle, ntest_image_per_style, nstyle])
+        style_variation[:, torch.arange(nstyle), :, torch.arange(nstyle)] = torch.linspace(-2, 2, ntest_image_per_style)
         latent_variation = torch.cat([one_hot_variation, style_variation], dim=-1)
-        latent_variation = latent_variation.reshape([self.nclasses * self.nstyle * self.ntest_image_per_style, 
-                                                     self.nclasses + self.nstyle])
+        latent_variation = latent_variation.reshape([nclasses * nstyle * ntest_image_per_style, 
+                                                     nclasses + nstyle])
         return latent_variation
 
-    def get_style_image_variation_plot(self, image_list: np.ndarray):
+    @staticmethod
+    def get_style_image_variation_plot(image_list: np.ndarray, nclasses, nstyle, ntest_image_per_style, base_title=""):
         assert isinstance(image_list, np.ndarray)
-        image_list = image_list.reshape((self.nclasses, self.nstyle, self.ntest_image_per_style) + self.image_dim)
+        image_list = image_list.reshape((nclasses, nstyle, ntest_image_per_style) + image_dim)
         fig_dict = dict()
-        for iclass in range(len(self.nclasses)):
+        for iclass in range(len(nclasses)):
             # noinspection PyTypeChecker
-            fig, ax_list = plt.subplots(self.nstyle, 1, sharex=True, sharey=True, figsize=(4, 4))
+            fig, ax_list = plt.subplots(nstyle, 1, sharex=True, sharey=True, figsize=(4, 4))
             for jstyle, (image_row, ax_row) in enumerate(zip(image_list[iclass], ax_list)):
                 for ktest, (image, ax) in enumerate(zip(image_row, ax_row)):
                     plt.imshow(image, extent=[1.6, 8.0, 180, 0], aspect=0.035, interpolation='spline36', cmap='jet')
                     if ktest == 0:
-                        ax.ylabel("$\\theta$")
-                    if jstyle == self.nstyle - 1:
+                        ax.ylabel(f"Style {jstyle + 1}\n" + "$\\theta$")
+                    if jstyle == nstyle - 1:
                         ax.xlabel('r ($\AA$)')
-            title = f"Image Variation CN{iclass + 4}"
+            title = f"{base_title}Image Variation CN{iclass + 4}"
             fig.suptitle(title, y=0.91)
             fig_dict[title] = fig
         return fig_dict
@@ -96,7 +98,8 @@ class Latent2AngularPDFTrainer:
         reconn_solver: optim.Optimizer = opt_cls(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
         scheduler = ReduceLROnPlateau(reconn_solver, factor=self.sch_factor, patience=self.sch_patience, cooldown=0, threshold=0.01, 
                                       mode="min", verbose=self.verbose)
-        latent_variation = self.precompute_latent_variation().to(self.device)
+        latent_variation = self.precompute_latent_variation(self.nclasses, self.nstyle, self.ntest_image_per_style)
+        latent_variation = latent_variation.to(self.device)
 
         last_best = 0.0
         chkpt_dir = f"{self.work_dir}/checkpoints"
@@ -154,7 +157,7 @@ class Latent2AngularPDFTrainer:
             if epoch % self.plot_interval == 0 and self.verbose:
                 image_list = self.model(latent_variation)
                 image_list = image_list.clone().detach().cpu().numpy()
-                fig_dict = self.get_style_image_variation_plot(image_list)
+                fig_dict = self.get_style_image_variation_plot(image_list, self.nclasses, self.nstyle, self.ntest_image_per_style)
                 for title, fig in fig_dict.items():
                         self.tb_writer.add_figure(title, fig, global_step=epoch)
 
@@ -168,7 +171,7 @@ class Latent2AngularPDFTrainer:
     def from_data(cls, pkl_fn, igpu=0, batch_size=512, lr=1.0E-3, max_epoch=300,  sch_factor=0.25, sch_patience=300, 
                   style_noise=0.01, weight_decay=1e-2, optimizer_name="AdamW", verbose=True, work_dir='.',
                   dropout_rate=0.05):
-        dl_train, dl_val, dl_test = get_latent2apdf_dataloaders(pkl_fn, batch_size)
+        dl_train, dl_val, _ = get_latent2apdf_dataloaders(pkl_fn, batch_size)
         
         use_cuda = torch.cuda.is_available()
         if use_cuda:
@@ -186,4 +189,25 @@ class Latent2AngularPDFTrainer:
         trainer = Latent2AngularPDFTrainer(model, device, dl_train, dl_val, lr=lr, max_epoch=max_epoch, sch_factor=sch_factor,
                                            sch_patience=sch_patience, style_noise=style_noise, weight_decay=weight_decay, 
                                            optimizer_name=optimizer_name, verbose=verbose, work_dir='.')
-                                           
+
+    @classmethod
+    def test_models(cls, work_dir='.', final_model_name='final.pt', best_model_name='best.pt'):
+        final_model = torch.load(f'{work_dir}/{final_model_name}', map_location=torch.device('cpu'))
+        best_model = torch.load(f'{work_dir}/{best_model_name}', map_location=torch.device('cpu'))
+
+        def plot_variation(model: Latent2AngularPDFTrainer, title='final '):
+            model.eval()
+            nstyle = model.nstyle
+            nclasses = model.nclasses
+            ntest_image_per_style = model.ntest_image_per_style
+            latent = cls.precompute_latent_variation(nclasses, nstyle, ntest_image_per_style)
+            image_list = model(latent).cpu().detach().numpy()
+            fig_dict = cls.get_style_image_variation_plot(image_list, nclasses, nstyle, ntest_image_per_style, title)
+            report_dir = os.path.join(work_dir, "reports")
+            if not os.path.exists(report_dir):
+                os.makedirs(report_dir)
+            for title, fig in fig_dict.values():
+                fig.savefig(f'{report_dir}/{title}.pdf', dpi=300)
+
+        plot_variation(final_model, title='Final ')
+        plot_variation(best_model, title='Best ')
