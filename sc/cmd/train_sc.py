e56e6b680db75df9e4afd007acbec7726a3887ae
Xiaohui Qu <xhqu1981@gmail.com>
2021-07-13 14:41:19 -0400

    make train_sc parallel

--- sc/cmd/train_sc.py
+++ sc/cmd/train_sc.py
@@ -6,7 +6,40 @@ import os
 import yaml
 import datetime
 import socket
+import ipyparallel as ipp
 
+def get_parallel_map_func():
+    c = ipp.Client(url_file="ipypar/security/ipcontroller-client.json")
+    print("Engine IDs:", c.ids)
+    with c[:].sync_imports():
+        from sc.clustering.trainer import Trainer
+        import os
+        import yaml
+        import datetime
+        import socket
+    c[:].push(dict(crop_image=run_training),
+              block=True)
+
+    return c[:].map_sync, len(c.ids)
+
+def run_training(job_number, work_dir, trainer_config, max_epoch, verbose, data_file, ngpus_per_node):
+    local_id = os.environ.get("SLURM_LOCALID", 0)
+    igpu = local_id % ngpus_per_node
+    trainer = Trainer.from_data(data_file,
+                                igpu=igpu,
+                                max_epoch=max_epoch,
+                                verbose=verbose,
+                                work_dir=f'{work_dir}/{job_number+1}',
+                                **trainer_config)
+    t1 = datetime.datetime.now()
+    print(f"Training started at {t1} on {socket.gethostname()}")
+    metrics = trainer.train()
+    t2 = datetime.datetime.now()
+    print('training finished at', t2)
+    print(f"Total {(t2 - t1).seconds + (t2 - t1).microseconds * 1.0E-6 :.2f}s used in traing")
+    print(metrics)
+    n_coord_num = trainer_config.get("n_coord_num", 3)
+    trainer.test_models(data_file, n_coord_num=n_coord_num, work_dir=work_dir)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,31 +55,40 @@ def main():
                         help="Working directory to write the output files")
     parser.add_argument('-g', '--gpu_i', type=int, default=-1,
                         help='ID for GPU to use')
+    parser.add_argument('--ngpus_per_node', type=int, default=0,
+                        help='Number of GPUs on each node')
+    parser.add_argument('--trials', type=int, default=1,
+                        help='Total number of trainings to run')                    
     args = parser.parse_args()
 
     work_dir = os.path.expandvars(os.path.expanduser(args.work_dir))
+    work_dir = os.path.abspath(work_dir)
     with open(os.path.expandvars(os.path.expanduser(args.config))) as f:
         trainer_config = yaml.full_load(f)
 
     if not os.path.exists(work_dir):
         os.makedirs(work_dir, exist_ok=True)
+    
+    max_epoch = args.max_epoch
+    verbose = args.verbose
+    data_file = os.path.abspath(os.path.expandvars(os.path.expanduser(args.data_file)))
+    trails = args.trials
+    ngpus_per_node = args.ngpus_per_node
 
-    trainer = Trainer.from_data(os.path.expandvars(os.path.expanduser(args.data_file)),
-                                igpu=args.gpu_i,
-                                max_epoch=args.max_epoch,
-                                verbose=args.verbose,
-                                work_dir=work_dir,
-                                **trainer_config)
-    t1 = datetime.datetime.now()
-    print(f"Training started at {t1} on {socket.gethostname()}")
-    metrics = trainer.train()
-    t2 = datetime.datetime.now()
-    print('training finished at', t2)
-    print(f"Total {(t2 - t1).seconds + (t2 - t1).microseconds * 1.0E-6 :.2f}s used in traing")
-    print(metrics)
-    n_coord_num = trainer_config.get("n_coord_num", 3)
-    trainer.test_models(args.data_file, n_coord_num=n_coord_num, work_dir=work_dir)
+    if trails > 1:
+        par_map, nprocesses = get_parallel_map_func()
+    else:
+        par_map, nprocesses = map, 1
+    print("running with {} processes".format(nprocesses))
 
+    par_map(run_training,
+            list(range(trails)), 
+            [work_dir]*trails, 
+            [trainer_config]*trails, 
+            [max_epoch]*trails, 
+            [verbose]*trails, 
+            [data_file]*trails, 
+            [ngpus_per_node]*trails)
 
 if __name__ == '__main__':
     main()
