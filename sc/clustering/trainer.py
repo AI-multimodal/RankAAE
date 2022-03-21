0e4c921312e29a55642b564d0e143d5a71efa8d4
Xiaohui Qu <xq@earth.org>
2022-03-21 11:08:07 -0400

    fix dimension match

--- sc/clustering/trainer.py
+++ sc/clustering/trainer.py
@@ -302,7 +302,7 @@ class Trainer:
                 assert len(z_aux.size()) == 2
                 aux_pred = z_aux[:, np.newaxis, :] - z_aux[np.newaxis, :, :]
                 aux_len = aux_pred.size()[0]
-                aux_loss = - (self.aux_weights[np.newaxis, :, np.newaxis] * aux_pred * aux_target).sum() / (
+                aux_loss = - (self.aux_weights[np.newaxis, np.newaxis, :] * aux_pred * aux_target).sum() / (
                     (aux_len**2 - aux_len) * n_aux)
                 loss_dict = {"Aux": aux_loss.item()}
                 if self.verbose:
