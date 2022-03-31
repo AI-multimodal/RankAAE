dc9db67660dd6b55ad04ecbfd47d43cef24c5d69
Zhu <zhul9311@gmail.com>
2022-03-31 12:35:26 -0400

    make Parameters object immutable

--- rank_aae/utils/parameter.py
+++ rank_aae/utils/parameter.py
@@ -1,6 +1,3 @@
-from collections import namedtuple
-
-
 from rank_aae.clustering.model import (
     CompactDecoder, 
     CompactEncoder, 
@@ -31,50 +28,35 @@ AE_CLS_DICT = {
     }
 }
 
-class parameters(namedtuple('Point', ['x', 'y'])):
-    def __init__(self):
-        
-
 
 class Parameters():
     """
     A parameter object that maps all dictionary keys into its name space.
     """
-    def __init__(self):
+    def __init__(self, parameter_dict):
         
+        self.update(parameter_dict)
+
+
+    def __setattr__(self, __name, __value):
         """
-        self, 
-        encoder, decoder, discriminator, device, train_loader, val_loader,
-        max_epoch=300, verbose=True, work_dir='.',
-        tb_logdir="runs", base_lr=0.0001,
+        The attributes are immutable, they can only be updated using `update` method.
         """
-        self.nstyle = 2,
-        self.batch_size = 111, 
-        self.grad_rev_beta = 1.1,  
-        self.alpha_flat_step = 100, 
-        self.alpha_limit = 2.0,
-        self.sch_factor = 0.25, 
-        self.sch_patience = 300, 
-        self.spec_noise = 0.01, 
-        self.weight_decay = 1e-2,
-        self.lr_ratio_Reconn = 2.0, 
-        self.lr_ratio_Mutual = 3.0, 
-        self.lr_ratio_Smooth = 0.1,
-        self.lr_ratio_Corr = 0.5, 
-        self.lr_ratio_Style = 0.5, 
-        self.optimizer_name = "AdamW",
-        self.aux_weights = None, 
-        self.kendall_activation = False,
-        self.use_flex_spec_target = False
-    
+        raise TypeError('Parameters object cannot be modified after instantiation')
+
     def get(self, key, value):
         """
         Override the get method in the original dictionary parameters.
         """
-        self.parameter_dict.get(key, value)
+        return self.__dict__.get(key, value)
     
+
     def update(self, parameter_dict):
+        """
+        The namespace can only be updated using this method.
+        """
         self.__dict__.update(parameter_dict) # map keys to its name space. 
+
 
     @classmethod
     def from_yaml(cls, config_file_path):
