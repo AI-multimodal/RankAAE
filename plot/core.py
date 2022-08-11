b56a4d27cbfc91ff60f8abdd5e6bde2920a2b70c
Zhu <zhul9311@gmail.com>
2022-08-11 11:59:58 -0400

    Delete unused modules and move reports to "plot" module

---
+++ plot/core.py
@@ -0,0 +1,260 @@
+import torch
+import numpy as np
+import pickle
+from collections import OrderedDict
+from matplotlib import pyplot as plt
+import os
+import argparse
+import json
+from sc.utils.parameter import Parameters
+from sc.clustering.dataloader import AuxSpectraDataset
+from plot import utils, utils_new
+
+def sorting_algorithm(x):
+    """
+    columns of `x` respresents: 
+        "Inter-style Corr", # 0
+        "Reconstion Err", # 1
+        "Style-Descriptor Corr 1", # 2
+        "Style-Descriptor Corr 2", # 3
+        "Style-Descriptor Corr 3", # 4
+        "Style-Descriptor Corr 4", # 5
+        "Style-Descriptor Corr 5" # 6
+    """
+
+    weight = [-1, 0, 1, 1, 1, 1, 1]
+
+    # if only weight[1] is non zero, turn on offset so the final score is non zero.
+    off_set = 0 
+    if np.sum(weight) == weight[1]:
+        off_set = 1
+    xx = x.copy()
+    xx[:,0] = x[:,0] *  weight[0] # Inter-style Corr
+    xx[:,1] = x[:,1] ** weight[1] # Reconstion Err
+    xx[:,2] = x[:,2] *  weight[2] # Style1 - CT Corr
+    xx[:,3] = x[:,3] *  weight[3] # Style2 - CN Corr
+    xx[:,4] = x[:,4] *  weight[4] # Style3 - OCN Corr
+    xx[:,5] = x[:,5] *  weight[5] # Style4 - Rstd Corr
+    xx[:,6] = x[:,6] *  weight[6] # Style5 - MOOD Corr
+    
+    
+    return (off_set + xx[:,0] + np.sum(xx[:,2:], axis=1)) / xx[:,1]
+
+
+def plot_report(test_ds, model, n_aux=5, title='report', device = torch.device("cpu")):
+
+    if n_aux == 5:
+        name_list = ["CT", "CN", "OCN", "Rstd", "OO"]
+    elif n_aux == 3:
+        name_list = ["BVS", "CN", "OCN"]
+
+    encoder = model['Encoder']
+    decoder = model['Decoder']
+    result = utils.evaluate_model(test_ds, model, device=device)
+    style_correlation = result["Inter-style Corr"]
+    
+    test_spec = torch.tensor(test_ds.spec, dtype=torch.float32, device=device)
+    test_grid = test_ds.grid
+    test_styles = encoder(test_spec).clone().detach().cpu().numpy()
+    descriptors = test_ds.aux
+
+    # generate a figure object to host all the plots
+    fig = plt.figure(figsize=(12,24),constrained_layout=True)
+    gs = fig.add_gridspec(12,6)
+    ax1 = fig.add_subplot(gs[0:2,0:2])
+    ax2 = fig.add_subplot(gs[0:2,2:4])
+    axa = fig.add_subplot(gs[0:2,4:6])
+    ax3 = fig.add_subplot(gs[2:4,0:2])
+    ax4 = fig.add_subplot(gs[2:4,2:4])
+    axb = fig.add_subplot(gs[2:4,4:6])
+    ax5 = fig.add_subplot(gs[4:6,4:6])
+    ax6 = fig.add_subplot(gs[6:8,4:6])
+    ax7 = fig.add_subplot(gs[8:10,4:6])
+
+    fig.suptitle(
+        f"{title:s}\n"\
+        f"Least correlation: {style_correlation:.4f}"
+    )
+    
+    # Plot out synthetic spectra variation
+    axs_spec = [ax1, ax2, axa, ax3, ax4, axb]
+    for istyle, ax in enumerate(axs_spec):
+        _ = utils.plot_spectra_variation(
+            decoder, istyle, 
+            true_range = True,
+            styles = test_styles,
+            amplitude = 2,
+            n_spec = 50, 
+            n_sampling = 5000, 
+            device = device,
+            energy_grid = test_grid, 
+            ax = ax
+        )
+
+    # Plot out descriptors vs styles
+    styles_no_s2 = np.delete(test_styles,1, axis=1)
+    descriptors_no_cn = np.delete(descriptors, 1, axis=1)
+    name_list_no_cn = np.delete(name_list, 1, axis=0)
+    for row in [4,5,6,7]:
+        for col in [0,1,2,3]:
+            ax = fig.add_subplot(gs[row,col])
+            
+            # only correlated style has fitted line plotted.
+            if col == row-4: 
+                plot_fit = True
+            else:
+                plot_fit = False
+
+            # for the first style (CT) use polynomial as fitting
+            if col == 0:
+                result_choice = ["R2", "Spearman", "Quadratic"]
+            else:
+                result_choice = ["R2", "Spearman"]
+            
+            accuracy = utils.get_descriptor_style_correlation(
+                styles_no_s2[:,col], 
+                descriptors_no_cn[:,row-4], 
+                ax=ax,
+                choice = result_choice,
+                fit = plot_fit,
+            )
+         
+            ax.set_title(
+                f"{name_list_no_cn[row-4]}: " +
+                "{0:.2f}/{1:.2f}".format(accuracy["Linear"]["R2"], accuracy["Spearman"])
+            )
+
+    # Plot q-q plot of the style distribution
+    for col in [0,1,2,3]:
+        ax = fig.add_subplot(gs[8,col])
+        shapiro_statistic = utils.qqplot_normal(styles_no_s2[:,col], ax)
+        if col > 0: col += 1 # skip style 2 which is CN
+        ax.set_title(f'style_{col+1}: {shapiro_statistic:.2f}')
+    
+    ax = fig.add_subplot(gs[9,3])
+    shapiro_statistic = utils.qqplot_normal(test_styles[:,1], ax)
+    ax.set_title(f'style_2: {shapiro_statistic:.2f}')
+
+    # Plot out CN confusion matrix
+    _ = utils.get_confusion_matrix(descriptors[:,1].astype('int'), test_styles[:,1], [ax5, ax6, ax7])
+    
+    return fig
+    
+
+def save_evaluation_result(save_dir, file_name, model_results, save_spectra=False, top_n=5):
+    """
+    Input is a dictionary of result dictionaries of evaluate_model.
+    And file name to save the resul.
+    Information is saved to a txt file.
+    """
+    save_dict = OrderedDict()
+    sorted_top_n_jobs = list(range(top_n))
+    for job, result in model_results.items():
+        if result['Rank'] in sorted_top_n_jobs:
+            sorted_top_n_jobs[result['Rank']] = job
+    for job in sorted_top_n_jobs:
+        result = model_results[job]
+        save_dict[job] = {
+            k: v for k, v in result.items() if k not in ["Input", "Output"]
+        }
+        if (result['Rank'] == 0) and save_spectra:
+            spec_in = result["Input"]
+            spec_out = result["Output"]
+    with open(os.path.join(save_dir, file_name+'.json'), 'wt') as f:
+        f.write(json.dumps(save_dict))
+    np.savetxt(os.path.join(save_dir, file_name+'.out'),spec_out)
+    np.savetxt(os.path.join(save_dir, file_name+'.in'),spec_in)
+
+
+def save_model_evaluations(save_dir, file_name, result):
+    with open(os.path.join(save_dir, file_name+"_model_evaluation.pkl"), "wb") as f:
+        pickle.dump(result, f)
+
+
+def save_report_plot(save_dir, file_name, fig):
+    fig.savefig(
+        os.path.join(save_dir, file_name+"_best_model.png"),
+        bbox_inches='tight'
+    )
+
+
+def save_model_selection_plot(save_dir, file_name, fig):
+    fig.savefig(
+        os.path.join(save_dir, file_name + "_model_selection.png"),
+        bbox_inches = 'tight'
+    )
+
+
+def main():
+    #### Parse arguments ####
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--work_dir', type = str, default = '.',
+                        help = "The folder where the model and data are.")  
+    parser.add_argument('-c', '--config', type=str, required=True,
+                        help='Config for training parameter in YAML format')
+    
+    args = parser.parse_args()
+    work_dir = os.path.abspath(os.path.expanduser(args.work_dir))
+    config = Parameters.from_yaml(os.path.join(work_dir, args.config))
+    
+
+    jobs_dir = os.path.join(work_dir, "training")
+    file_name = config.data_file
+    
+    device = torch.device("cpu") # device is cpu by default
+    if config.gpu: 
+        try:
+            device = torch.device("cuda:0")
+        except:
+            device = torch.device("cpu")
+
+    #### Create test data set from file ####
+    if file_name == None:  # if datafile name nor provided, search for it.
+        data_file_list = [f for f in os.listdir(work_dir) if f.endswith('.csv')]
+        assert len(data_file_list) == 1, "Which data file are you going to use?"
+        file_name = data_file_list[0]
+    test_ds = AuxSpectraDataset(os.path.join(work_dir, file_name), split_portion = "val", n_aux = 5)
+    
+    #### Choose the 20 top model based on evaluation criteria ####
+    model_results = utils.evaluate_all_models(jobs_dir, test_ds, device=device) # models are not sorted
+
+    model_results, sorted_jobs, fig_model_selection = utils.sort_all_models( 
+        model_results, 
+        plot_score = True, 
+        top_n = config.top_n, 
+        sort_score = sorting_algorithm,
+        ascending = False, # best model has the highest score
+    ) # models are sorted
+    save_model_evaluations(work_dir, config.output_name, model_results)
+    
+    # genearte model selection scores plot
+    if fig_model_selection is not None:
+        save_model_selection_plot(work_dir, config.output_name, fig_model_selection)
+
+    # generate report for top model
+    top_model = torch.load(
+            os.path.join(jobs_dir, sorted_jobs[0], "final.pt"), 
+            map_location = device
+    )
+    fig_top_model = plot_report(
+        test_ds, 
+        top_model, 
+        n_aux = config.n_aux, 
+        title = '-'.join([config.output_name, sorted_jobs[0]]), 
+        device = device
+    )
+
+    save_report_plot(work_dir, config.output_name, fig_top_model)
+    recon_evaluator = utils_new.Reconstruct(name=config.output_name, device=device)
+    recon_evaluator.evaluate(test_ds, top_model, path_to_save=work_dir)
+    
+    # save top 5 result 
+    save_evaluation_result(work_dir, config.output_name, model_results, save_spectra=True, top_n=config.top_n)
+    
+    plotter = utils_new.LossCurvePlotter()
+    fig = plotter.plot_loss_curve(os.path.join(jobs_dir, sorted_jobs[0], "losses.csv"))
+    fig.savefig("loss_curves.png", bbox_inches="tight")
+    print("Success: training report saved!")
+
+if __name__ == "__main__":
+    main()
---
+++ plot/utils_new.py
@@ -0,0 +1,280 @@
+import os
+import torch
+from datetime import datetime
+from uuid import uuid4
+import numpy as np
+import pandas as pd
+from monty.json import MSONable
+
+from scipy.interpolate import interp1d
+
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+
+def create_plotly_colormap(n_colors):
+    '''
+    Xiaohui's implementation of getting spectra color map.
+    '''
+    plotly3_hex_strings = px.colors.sequential.Plotly3
+    rgb_values = np.array([[int(f"0x{c_hex[i:i+2]}", 16) for i in range(1, 7, 2)] for c_hex in plotly3_hex_strings])
+    x0 = np.linspace(1, n_colors, rgb_values.shape[0])
+    x1 = np.linspace(1, n_colors, n_colors)
+    target_rgb_values = np.stack([interp1d(x0, rgb_values[:, i], kind='cubic')(x1) for i in range(3)]).T.round().astype('int')
+    target_rgb_strings = ["#"+"".join([f'{ch:02x}' for ch in rgb]) for rgb in target_rgb_values]
+    return target_rgb_strings
+
+
+class Reporter:
+    """
+    A class to evaluate training job (for all sub jobs).
+    """
+    def __init__(self):
+        pass
+    
+    def add_evaluations(self,evaluation_list):
+        for evaluation in evaluation_list:
+            pass
+        
+    def evaluate_all_models(self, training_path='./training'):
+        """
+        Evaluate all the models saved in the training_path (which is './training' by default)
+        """
+    def load_evaluations(self):
+        """
+        Evaluate results from the models, if the results exist alrady, collect them.
+        """
+        pass
+    def report(self):
+        """
+        Print (& plot) a report for all the model evaluations.
+        """
+        pass
+
+
+
+class Evaluator(MSONable):
+    """
+    A base class for evluating a specific property/score of a model.
+    """
+    def __init__(self, device=torch.device('cpu'), name=None):
+        
+        self.result = {}
+        self.metadata = {}
+        self.device = device
+        self.name = name # the key that will be shown in the final Result dictionary.
+
+
+    def evaluate(self, *args, **kwargs):
+        """
+        Evaluate the model. Returns a dictionary of evaluated metrics.
+        """
+        raise NotImplementedError
+    
+    def _process_metadata(self, data_path=None, model_path=None):
+        """Create metadata for the evaluation.
+        """
+        dt = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        self.metadata.update(
+            {
+                "name": self.name,
+                "datetime": f"{dt} UTC",
+                "data": data_path,
+                "model": model_path
+            }
+        )
+        
+
+    def plot(self, ax=None):
+        """Plot out the evaluated result.
+        """
+        raise NotImplementedError
+
+
+class Reconstruct(Evaluator):
+    def __init__(self, device=torch.device('cpu'), name="reconstructed"):
+        super(Reconstruct, self).__init__(device=device, name=name)
+        
+    def evaluate(self, test_ds, model, path_to_save=None):
+        """
+        Parameters
+        ----------
+        eval_ds : dataset used for evaluation (either validation or test dataset)
+        model : the NN model to be evaluated.
+        """
+        self._process_metadata(test_ds.metadata["path"], model_path=None)
+        encoder = model['Encoder']
+        decoder = model['Decoder']
+        encoder.eval()
+        decoder.eval()
+
+        spec_in = torch.tensor(test_ds.spec, dtype=torch.float32, device=self.device)
+        styles = encoder(spec_in)
+        
+        self.result.update(
+            {
+                "input": spec_in.cpu().numpy(),
+                "styles": styles.clone().detach().cpu().numpy(),
+                "output": decoder(styles).clone().detach().cpu().numpy()
+            }
+        )
+
+        if path_to_save is not None:
+            self.to_file(path_to_save)        
+    
+    def to_file(self, path_to_save):
+        file_path = os.path.join(path_to_save, self.name)
+        np.savetxt(file_path+"_spec_in"+".txt", self.result["input"])
+        np.savetxt(file_path+"_spec_out"+".txt", self.result["output"])
+        np.savetxt(file_path+"_styles"+".txt", self.result["styles"])
+
+
+class EvaluatorAll:
+    """
+    A base class to evaluate one model and produces a result dicionary.
+    """
+    def __init__(self, device=torch.device('cpu')):
+        self.data = None,
+        self.model = None,
+        self.device = device,
+    
+    @classmethod
+    def from_file(cls, data_path=None, model_path=None, device=torch.device('cpu')):
+        """
+        Initate an Evaluator instance from a model file and validation dataset.
+        """
+        eval = Evaluator(device=device)
+        eval.load_data(data_path)
+        eval.load_model(model_path)
+
+        return eval
+    
+    def load_model(self, model_path=None):
+        """
+        Load model from a model file.
+        """
+        pass
+
+    def load_data(self, data_path=None):
+        """
+        Load validation/testing data from a data path.
+        """
+        pass
+
+
+
+class SpectraVariationEvaluator(Evaluator):
+    
+    def __init__(self, 
+        n_spec=50, 
+        n_sampling=1000, 
+        amplitude = 2,
+        device=torch.device('cpu')):
+        
+        super().__init__(device=device)
+        self.n_spec = n_spec
+        self.n_sampling = n_sampling
+        self.amplitude = amplitude
+        self.styles = None
+        self.istyle = None
+        self.model = None
+
+
+    def evaluate(self, istyle, true_range = True):
+        """Spectra variation plot by varying one of the styles.
+        Parameters
+        ----------
+        istyle : int
+            The column index of `styles` for which the variation is plotted.
+        true_range : bool
+            If True, sample from the 5th percentile to 95th percentile of a style, instead of 
+            [-amplitude, +amplitude].
+        amplitude : float
+            The range from which styles are sampled. Effective only if `true_range` is False.
+        style : array_like
+            2-D array of complete styles. Effective and can't be None if `true_range` evaluates.
+            True. The `istyle`th column 
+        """
+        decoder = self.model['Decoder']
+        decoder.eval()
+        
+        if self.n_sampling == None: 
+            c = np.linspace(*[-self.amplitude, self.amplitude], self.n_spec)
+            c2 = np.stack([np.zeros_like(c)] * istyle + [c] + [np.zeros_like(c)] * (decoder.nstyle - istyle - 1), axis=1)
+            con_c = torch.tensor(c2, dtype=torch.float, requires_grad=False)
+            spec_out = decoder(con_c).reshape(self.n_spec, -1).clone().cpu().detach().numpy()
+
+        else:
+            # Create a 3-D array whose x,y,z dimensions correspond to: style variation, n_sampling, 
+            # and number of styles. 
+            con_c = torch.randn([self.n_spec, self.n_sampling, decoder.nstyle], device = self.device)
+            if true_range: # sample from the true range of a style 
+                assert len(self.styles.shape) == 2 # styles must be a 2-D array
+                style_range = np.percentile(self.styles[:, istyle], [5, 95])
+                style_variation = torch.linspace(*style_range, self.n_spec, device = self.device)
+            else: # sample from the absolute range
+                style_variation = torch.linspace(-self.amplitude, self.amplitude, self.n_spec, device = self.device)
+            # Assign the "layer" to be duplicates of `style_variation`
+            con_c[..., istyle] = style_variation[:, np.newaxis]
+            con_c = con_c.reshape(self.n_spec * self.n_sampling, decoder.nstyle)
+            spec_out = decoder(con_c).reshape(self.n_spec, self.n_sampling, 256)
+            # Average along the `n_sampling` dimsion.
+            self.result = spec_out.mean(axis = 1).cpu().detach().numpy()
+            self.istyle = istyle
+
+    def plot(self, ax = None, energy_grid = None):
+        
+        assert self.istyle is not None, "Please evaluate first!"
+        colors = create_plotly_colormap(self.n_spec)
+        
+        if ax is None: # create a standalone fig
+            fig, ax = plt.subplot(figsize=(8,6))
+        else:
+            fig, ax = None, ax
+        
+        for spec, color in zip(self.result, colors):
+            if energy_grid is None:
+                ax.plot(spec, lw=0.8, c=color)
+            else: 
+                ax.plot(energy_grid, spec, lw=0.8, c=color)
+        
+        ax.set_title(f"Varying Style #{self.istyle+1}", y=1)
+        
+        return fig
+
+
+class LossCurvePlotter(MSONable):
+    def __init__(self):
+        self.loss_names = ["D", "G", "Aux", "Recon", "Smooth", "Mutual_Info"]
+        self.loss_dict = {name: {} for name in self.loss_names}
+        self.epochs = None
+
+    def _load_losses(self, file_path):
+        self.loss_df = pd.read_csv(file_path, index_col=False, delimiter=",")
+        # self.loss_df.columns = ["_".join(col.split("_")[1:]) for col in self.loss_df.columns.tolist()]
+        self.epochs = self.loss_df.iloc[:,0].to_numpy()
+        for name in self.loss_names:
+            self.loss_dict[name]["Train"] = self.loss_df.loc[:, f"Train_{name}"].to_numpy()
+            self.loss_dict[name]["Val"] = self.loss_df.loc[:, f"Val_{name}"].to_numpy()
+
+    def plot_loss_curve(self, file_path):
+        self._load_losses(file_path)
+        fig, axs = plt.subplots(6, 1, figsize=(6,15), dpi=150)
+        i = 0
+        for name, loss in self.loss_dict.items():
+            if name == "Epochs": continue
+            axs[i].plot(
+                self.epochs, loss["Train"], 
+                label=f"Train:{loss['Train'][-10:].mean():.4f}",
+                lw=0.8, alpha=1
+            )
+            axs[i].plot(
+                self.epochs, loss["Val"], 
+                label=f"Val:{loss['Val'][-10:].mean():.4f}", 
+                lw=0.8, alpha=0.5
+            )
+            axs[i].set_title(name, y=1.0, pad=-14)
+            axs[i].tick_params(axis="both", direction="in")
+            axs[i].legend()
+            i += 1
+        return fig
