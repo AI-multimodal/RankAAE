59fd33736ffbe59160e3fd2fd51a01d6ff88ecde
Xiaohui Qu <xq@earth.org>
2022-08-12 13:17:56 -0400

    update bash script file to be compatible with train_sc and generate_report

--- sc/cmd/run_locally.sh
+++ sc/cmd/run_locally.sh
@@ -16,43 +16,35 @@ total_engines=16
 total_jobs=100
 
 element=$1
-work_dir="../tests/work_dir_$1"
-cd ${work_dir}
+work_dir="work_dir_$1"
 data_file=$(ls feff_$1_*)
 echo ${data_file}
 rm -r training
 
-ipython profile create --profile-dir=ipypar
-ipcontroller --ip="*" --profile-dir=ipypar &
+ipython profile create --profile-dir=${work_dir}/ipypar
+ipcontroller --ip="*" --profile-dir=${work_dir}/ipypar &
 sleep 10
 
 for i in `seq 1 $total_engines`
 do
-export SLURM_LOCALID=$i
-ipengine --profile-dir=ipypar --log-to-file &
+    export SLURM_LOCALID=$i
+    ipengine --profile-dir=${work_dir}/ipypar --log-to-file &
 done
 
-wait_ipp_engines -e $total_engines
+wait_ipp_engines -w ${work_dir} -e $total_engines
 echo "Engines seems to have started"
 
 echo `date` "Start training"
-train_sc \
-    -d ${data_file} \
-    --trials ${total_jobs} \
-    -c fix_config.yaml \
-    -e 1500 \
-    -v
+echo train_sc -w ${work_dir} -c fix_config.yaml
+train_sc -w ${work_dir} -c fix_config.yaml
 echo `date` "Job Finished"
 stop_ipcontroller
-rm -r ipypar
+rm -r ${work_dir}/ipypar
 
 echo `date` "Genearting Report"
 current_folder=$(basename `pwd`)
 parent_folder=$(basename $(dirname `pwd`))
-sc_generate_report \
-    -o report_${parent_folder}_${current_folder} \
-    -n 5 \
-    -p 5 \
-    -g 
+echo sc_generate_report -w ${work_dir} -c fix_config.yaml
+sc_generate_report -w ${work_dir} -c fix_config.yaml
 echo `date` "Report Generated"
 
